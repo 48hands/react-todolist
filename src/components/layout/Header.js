62d6afa4ef977b25ca6cac45caeb3e37d51d33e5
chocomintkusoyaro <naruhodo.u1@icloud.com>
2019-08-31 00:36:47 +0900

    init

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import config from 'config';
 
 export default function Header() {
   return (
     <header style={headerStyle}>
-      <h1>TodoList </h1>
+      <h1>TodoList</h1>
       <Link style={linkStyle} to="/">
         Home
       </Link>{' '}
